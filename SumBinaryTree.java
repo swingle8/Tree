65d005df496bb90a022227a719833ed3749e151e
Shivam Singhal <swingle8@gmail.com>
2020-09-09 22:47:31 +0530

    Add files via upload

---
+++ SumBinaryTree.java
@@ -0,0 +1,96 @@
+// Sum binary tree or not
+// Problem Description
+
+// Given a binary tree. Check whether the given tree is a Sum-binary Tree or not.
+
+// Sum-binary Tree is a Binary Tree where the value of a every node is equal to sum of the nodes present in its left subtree and right subtree.
+
+// An empty tree is Sum-binary Tree and sum of an empty tree can be considered as 0. A leaf node is also considered as SumTree.
+
+// Return 1 if it sum-binary tree else return 0.
+
+
+
+// Problem Constraints
+// 1 <= length of the array <= 100000
+
+// 0 <= node values <= 50
+
+
+
+// Input Format
+// The only argument given is the root node of tree A.
+
+
+
+// Output Format
+// Return 1 if it is sum-binary tree else return 0.
+
+
+
+// Example Input
+// Input 1:
+
+//        26
+//      /    \
+//     10     3
+//    /  \     \
+//   4   6      3
+// Input 2:
+
+//        26
+//      /    \
+//     10     3
+//    /  \     \
+//   4   6      4
+
+
+// Example Output
+// Output 1:
+
+//  1
+// Output 2:
+
+//  0
+
+
+// Example Explanation
+// Explanation 1:
+
+//  All leaf nodes are considered as SumTree. 
+//  Value of Node 10 = 4 + 6.
+//  Value of Node 3 = 0 + 3 
+//  Value of Node 26 = 20 + 6 
+// Explanation 2:
+
+//  Sum of left subtree and right subtree is 27 which is not equal to the value of root node which is 26.
+
+
+
+
+public class Solution {
+    int ans = 1;
+    public int solve(TreeNode A) {
+      Bsum(A);
+      return ans;
+    }
+    
+    public int Bsum(TreeNode A) {
+        if (A == null)
+            return 0;
+            
+        int left = Bsum(A.left);
+        int right = Bsum(A.right);
+        
+        if ((left + right) == A.val)
+            return 2*A.val;
+        
+        else if (left == 0 && right == 0)
+            return A.val;
+        else
+            ans = 0;
+        
+        return ans;
+        
+    }
+}
